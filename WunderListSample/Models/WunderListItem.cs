d5159f8d8456a2170d4201ef6c9c3154da893cba
v-keshen <809276891@qq.com>
2015-06-05 16:52:37 +0800

    Add

---
+++ WunderListSample/Models/WunderListItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WunderListSample.Models
+{
+    public class WunderListItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+
+        public string Owner_Type { get; set; }
+
+        public string Owner_Id { get; set; }
+
+        public string List_Type { get; set; }
+
+        public WunderListType List_Type { get; set; }
+
+        public string Revision { get; set; }
+
+        public string Created_At{ get; set; }
+
+    }
+}
---
+++ WunderListSample/Models/WunderTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WunderListSample.Models
+{
+    public class WunderTask
+    {
+        public string Id { get; set; }
+        public string CreateDateTime { get; set; }
+
+        //Refer to ListItem Id
+        public string ListId { get; set; }
+
+        public string Title { get; set; }
+    }
+}
